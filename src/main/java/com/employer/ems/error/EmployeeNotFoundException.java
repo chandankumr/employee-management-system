7d713b901640f4c3a7679054ddb15ef2cab550c9
chandankumr <69294158+chandankumr@users.noreply.github.com>
2023-08-20 20:16:59 +0530

    Delete src directory

--- src/main/java/com/employer/ems/error/EmployeeNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.employer.ems.error;
-
-public class EmployeeNotFoundException extends RuntimeException {
-
-    public EmployeeNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/employer/ems/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.employer.ems.repository;
-
-import com.employer.ems.entity.EmployeeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<EmployeeEntity,String> {
-}
